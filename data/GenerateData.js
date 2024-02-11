9451741cca1edb45da7dafbeb6f3ceed6bf5e8d7
Noob_Hacker <gauravjais4491@gmail.com>
2024-02-11 18:39:15 +0530

    add faker in project

---
+++ data/GenerateData.js
@@ -0,0 +1,21 @@
+const faker = require('faker');
+class GenerateData {
+    constructor(page) {
+        this.page = page
+        this.usernameRegex = /^[a-zA-Z0-9._-]{2,50}$/;
+    }
+    static createInstance() {
+        return new GenerateData()
+    }
+    async generateGivenName() {
+        let randomName = faker.name.findName();
+        let isValidUsername = this.usernameRegex.test(randomName);
+        while (!isValidUsername) {
+            randomName = faker.internet.userName().toLowerCase();
+            isValidUsername = this.usernameRegex.test(randomName);
+        }
+        return randomName;
+    }
+}
+
+module.exports = GenerateData;
--- tests/createAnAccount.spec.js
+++ tests/createAnAccount.spec.js
@@ -3,11 +3,20 @@ const json = require('../data/userData.json')
 const userData = JSON.parse(JSON.stringify(json))
 const { customTest } = require('../fixtures/createAccount-Fixtures')
 const expectedString = require('../data/expectedStringData.json')
+const generateData = require('../data/GenerateData')
+
+let givenName
+
+customTest.beforeEach('should generate given name', async ({generateData}) => {
+    givenName = await generateData.generateGivenName();
+})
+
+
 
 customTest('should create an account', async ({ createNewAccount, notification }, testInfo) => {
     console.log(testInfo.title);
-    await createNewAccount.addPersonDetails(userData.FamilyName, userData.GivenName, userData.gender)
-    await createNewAccount.addUserAccountDetails(userData.GivenName, userData.privilegeLevelText, userData.password, userData.confirmPassword)
+    await createNewAccount.addPersonDetails(userData.FamilyName, givenName, userData.gender)
+    await createNewAccount.addUserAccountDetails(givenName, userData.privilegeLevelText, userData.password, userData.confirmPassword)
     await createNewAccount.addCapablitiesToUserAccount(userData.capabilities)
     await createNewAccount.addProviderDetails(userData.idenfier, userData.providerRoleText)
     await createNewAccount.saveDetailsBtn()
@@ -16,7 +25,7 @@ customTest('should create an account', async ({ createNewAccount, notification }
 
 customTest('should create an account with password less than eight character', async ({ createNewAccount, securePageForCreateNewAccount }, testInfo) => {
     console.log(testInfo.title);
-    await createNewAccount.addPersonDetails(userData.FamilyName, userData.GivenName, userData.gender)
+    await createNewAccount.addPersonDetails(userData.FamilyName, givenName, userData.gender)
     await createNewAccount.addUserAccountDetails(userData.GivenName, userData.privilegeLevelText, userData.passwordLessThanEightDigit.password, userData.passwordLessThanEightDigit.confirmPassword)
     expect.soft(await securePageForCreateNewAccount.flashPasswordLessThanEightCharacter()).toContain(expectedString.expectTextForPasswordLessThanEightCharacters)
 })
